a5eb16a3464beb097ce80523c8a1a81a526a9c93
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-08-31 07:55:58 -0300

    useEffect - React Hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : ' ';
+  const [counter2, setCounter2] = useState(0);
 
-  const handleClick = () => {
-    setReverse((reverse) => !reverse);
-  };
+  // o useEffect faz o trabalho de componentDidMount, componentDidUpdate e componentWillUnmount;
+  // componentDidUpdate - executa toda vez que o componente atualiza;
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
 
-  const handleCounter = () => {
-    // as funções de set podem receber uma função de callback;
-    // agora não utilizamos diretamente o counter utilizamos o parâmetro da função de callback que o setCounter recebe;
-    // em alguns casos é mais vatajoso utilizarmos uma função de callback, porque assim garantimos que temos o estado atual correto;
-    setCounter((c) => c + 1);
-  };
+  // componentDidMount - executa 1x;
+  // [] é um array de dependências e se passarmos um array vazio vai executar somente 1x;
+  // ? é chamado de encademento opcional;
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    // componentWillUnmount
+    // para limpar
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  // com dependência - executa toda vez que minha dependência mudar;
+  // eu só quero que essa função seja executada quando a minha dependência [] mudar, e por dependência estou falando de parte do estado;
+  useEffect(() => {
+    console.log('C1: ', counter, 'C2: ', counter2);
+  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Contador: {counter}</h1>
-
-        <button type="button" onClick={handleClick}>
-          Clique aqui para inverter a rotação da imagem {reverseClass}
-        </button>
-
-        <button type="button" onClick={handleCounter}>
-          Clique aqui para incrementar +1 ao contador
-        </button>
-      </header>
+      <p>Teste 2</p>
+      <h1>
+        C1: {counter} C2: {counter2}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+</button>
     </div>
   );
 }
