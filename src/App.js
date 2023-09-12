22e44d49e7630249bf63ac485147ad8233b77120
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-09-12 07:17:56 -0300

    useRef - React Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import P from 'prop-types';
 import './App.css';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou!');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,27 +18,43 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const counter = useRef(0);
 
   console.log('Pai renderizou!');
 
   //Component Did Mount
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    counter.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h1>Renderizou: {counter.current}x</h1>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -47,7 +63,9 @@ function App() {
       {useMemo(() => {
         return (
           posts.length > 0 &&
-          posts.map((post) => <Post key={post.id} post={post} />)
+          posts.map((post) => (
+            <Post key={post.id} post={post} handleClick={handleClick} />
+          ))
         );
       }, [posts])}
 
