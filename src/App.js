a00646a88d41be03fb55c0a27a9e4bc625b78560
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-09-21 12:08:44 -0300

    useReducer - React Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+import { useReducer } from 'react';
+
+const inicialState = {
+  title: 'O título do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  console.log('NENHUMA ACTION ENCONTRADA');
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, inicialState);
+  // função dispatch dispara ações
+  const { title, counter, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+      <button onClick={() => dispatch({ type: ' ' })}>Sem action</button>
+    </div>
   );
 }
 
